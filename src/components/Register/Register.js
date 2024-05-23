0038a3b36399d1ada2dad4274cc08ebfad96c327
moraakhila <moraakhila1@gmail.com>
2024-05-23 09:03:36 -0500

    Changed title of the App

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -89,7 +89,7 @@ class Register extends React.Component {
     return (
       <article
         className="br3 ba b--black-10 mt5 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center"
-        style={{ marginTop: "110px" }}
+        style={{ marginTop: "90px" }}
       >
         <main className="pa4 black-80">
           <div className="measure">
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -52,7 +52,7 @@ class Signin extends React.Component {
     return (
       <article
         className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 mt5 shadow-5 center"
-        style={{ marginTop: "110px" }}
+        style={{ marginTop: "90px" }}
       >
         <main className="pa4 black-80">
           <div className="measure">
